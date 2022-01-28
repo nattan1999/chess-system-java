4ae3e197695b6426406963c3f4ed0e4298928da0
Natan oliveira <natan.o@uni9.edu.br>
2022-01-27 21:01:11 -0300

    ChessExeception and ChessPosition

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
 import chess.ChessMatch;
-
+import chess.ChessPiece;
 
 public class Program {
 
@@ -9,6 +9,7 @@ public class Program {
 	 
 	ChessMatch chessMatch = new ChessMatch();
 	UI.printBoard(chessMatch.getPieces());
+		
 	
 	
 	
